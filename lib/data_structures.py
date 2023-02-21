ef45a356270e004087a49c4abc1f6a740ae75130
Bianca Aspin <bianca@mj94.net>
2023-02-21 15:15:03 -0800

    COMPLETE LAB>

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,5 @@
+""" import ipdb """
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -17,22 +19,43 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names = [food.get("name") for food in spicy_foods]
+    return names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spicy_foods = [food for food in spicy_foods if food.get("heat_level") > 5]
+    return spicy_foods
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food.get("name")
+        cuisine = food.get("cuisine")
+        heat = food.get("heat_level") * "🌶"
+        print(f"{name} ({cuisine}) | Heat Level: {heat}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food.get("cuisine") == cuisine:
+            return food
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    foods_to_print = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(foods_to_print)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat = [food.get("heat_level") for food in spicy_foods]
+    total_heat = sum(heat)
+    avg_heat = total_heat/len(heat)
+    return avg_heat
+
+spicy_food = {
+    "name": "Griot",
+    "cuisine": "Haitian",
+    "heat_level": 10,
+}
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
+""" ipdb.set_trace() """
